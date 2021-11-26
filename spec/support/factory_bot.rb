a45023002ea33a8637b09a61910b4911e07bbd73
Martin Villarreal Serna <martin.serna@gmail.com>
2021-11-26 12:45:56 -0500

    Add tests for books API

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -7,4 +7,10 @@ FactoryBot.define do
     password_confirmation { 'password' }
   end
 
+  factory :book do
+    sequence(:title) { |n| "Test Book #{n}" }
+    sequence(:author) { |n| "John Doe #{n}" }
+    sequence(:isbn) { |n| "100000000000#{n}" }
+    description { "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod ratione impedit temporibus maiores autem aperiam assumenda exercitationem, quisquam nobis esse." }
+  end
 end
